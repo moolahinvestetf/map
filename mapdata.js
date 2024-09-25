ff5e639f73383edcf47493271fd382707bce972d
moolahinvestetf <info@moolahinvest.com>
2024-09-25 11:41:45 +0200

    Add files via upload

---
+++ mapdata.js
@@ -0,0 +1,291 @@
+var simplemaps_europemap_mapdata={
+  main_settings: {
+   //General settings
+    width: "responsive", //'700' or 'responsive'
+    background_color: "#FFFFFF",
+    background_transparent: "yes",
+    border_color: "#ffffff",
+    popups: "detect",
+    
+    //State defaults
+    state_description: "State description",
+    state_color: "#88A4BC",
+    state_hover_color: "#3B729F",
+    state_url: "",
+    border_size: 1.5,
+    all_states_inactive: "no",
+    all_states_zoomable: "yes",
+    
+    //Location defaults
+    location_description: "Location description",
+    location_color: "#FF0067",
+    location_opacity: 0.8,
+    location_hover_opacity: 1,
+    location_url: "",
+    location_size: 25,
+    location_type: "square",
+    location_image_source: "frog.png",
+    location_border_color: "#FFFFFF",
+    location_border: 2,
+    location_hover_border: 2.5,
+    all_locations_inactive: "no",
+    all_locations_hidden: "no",
+    
+    //Label defaults
+    label_color: "#d5ddec",
+    label_hover_color: "#d5ddec",
+    label_size: 22,
+    label_font: "Arial",
+    hide_labels: "no",
+    manual_zoom: "yes",
+    back_image: "no",
+    arrow_color: "#cecece",
+    arrow_color_border: "#808080",
+    initial_back: "no",
+    initial_zoom: -1,
+    initial_zoom_solo: "no",
+    region_opacity: 1,
+    region_hover_opacity: 0.6,
+    zoom_out_incrementally: "yes",
+    zoom_percentage: 0.99,
+    zoom_time: 0.5,
+    
+    //Popup settings
+    popup_color: "white",
+    popup_opacity: 0.9,
+    popup_shadow: 1,
+    popup_corners: 5,
+    popup_font: "12px/1.5 Verdana, Arial, Helvetica, sans-serif",
+    popup_nocss: "no",
+    
+    //Advanced settings
+    div: "map",
+    auto_load: "yes",
+    url_new_tab: "no",
+    images_directory: "default",
+    fade_time: 0.1,
+    link_text: "View Website",
+    state_image_url: "",
+    state_image_position: "",
+    location_image_url: ""
+  },
+  state_specific: {
+    AL: {
+      name: "Albania",
+      color: "#04d3b4",
+      hover_color: "#c2acd4"
+    },
+    AM: {
+      name: "Armenia",
+      color: "#04d3b4",
+      hover_color: "#c2acd4"
+    },
+    AT: {
+      name: "Austria",
+      color: "#04d3b4",
+      hover_color: "#c2acd4"
+    },
+    BA: {
+      name: "Bosnia and Herzegovina",
+      color: "#04d3b4",
+      hover_color: "#c2acd4"
+    },
+    BE: {
+      name: "Belgium",
+      color: "#04d3b4",
+      hover_color: "#c2acd4"
+    },
+    BG: {
+      name: "Bulgaria",
+      color: "#04d3b4",
+      hover_color: "#c2acd4"
+    },
+    BY: {
+      name: "Belarus",
+      color: "#04d3b4",
+      hover_color: "#c2acd4"
+    },
+    CH: {
+      name: "Switzerland",
+      color: "#04d3b4",
+      hover_color: "#c2acd4"
+    },
+    CY: {
+      name: "Cyprus",
+      color: "#04d3b4",
+      hover_color: "#c2acd4"
+    },
+    CZ: {
+      name: "Czech Republic",
+      color: "#04d3b4",
+      hover_color: "#c2acd4"
+    },
+    DE: {
+      name: "Germany",
+      color: "#04d3b4",
+      hover_color: "#c2acd4"
+    },
+    DK: {
+      name: "Denmark",
+      color: "#04d3b4",
+      hover_color: "#c2acd4"
+    },
+    EE: {
+      name: "Estonia",
+      color: "#04d3b4",
+      hover_color: "#c2acd4"
+    },
+    ES: {
+      name: "Spain",
+      color: "#04d3b4",
+      hover_color: "#c2acd4"
+    },
+    FI: {
+      name: "Finland",
+      color: "#04d3b4",
+      hover_color: "#c2acd4"
+    },
+    FR: {
+      name: "France",
+      color: "#04d3b4",
+      hover_color: "#c2acd4"
+    },
+    GB: {
+      name: "United Kingdom",
+      color: "#04d3b4",
+      hover_color: "#c2acd4"
+    },
+    GE: {
+      name: "Georgia",
+      color: "#04d3b4",
+      hover_color: "#c2acd4"
+    },
+    GR: {
+      name: "Greece",
+      color: "#04d3b4",
+      hover_color: "#c2acd4"
+    },
+    HR: {
+      name: "Croatia",
+      color: "#04d3b4",
+      hover_color: "#c2acd4"
+    },
+    HU: {
+      name: "Hungary",
+      color: "#04d3b4",
+      hover_color: "#c2acd4"
+    },
+    IE: {
+      name: "Ireland",
+      color: "#04d3b4",
+      hover_color: "#c2acd4"
+    },
+    IS: {
+      name: "Iceland",
+      color: "#04d3b4",
+      hover_color: "#c2acd4"
+    },
+    IT: {
+      name: "Italy",
+      color: "#04d3b4",
+      hover_color: "#c2acd4"
+    },
+    LT: {
+      name: "Lithuania",
+      color: "#04d3b4",
+      hover_color: "#c2acd4"
+    },
+    LU: {
+      name: "Luxembourg",
+      color: "#04d3b4",
+      hover_color: "#c2acd4"
+    },
+    LV: {
+      name: "Latvia",
+      color: "#04d3b4",
+      hover_color: "#c2acd4"
+    },
+    MD: {
+      name: "Moldova",
+      color: "#04d3b4",
+      hover_color: "#c2acd4"
+    },
+    ME: {
+      name: "Montenegro",
+      color: "#04d3b4",
+      hover_color: "#c2acd4"
+    },
+    MK: {
+      name: "Macedonia",
+      color: "#04d3b4",
+      hover_color: "#c2acd4"
+    },
+    NL: {
+      name: "Netherlands",
+      color: "#04d3b4",
+      hover_color: "#c2acd4"
+    },
+    NO: {
+      name: "Norway",
+      color: "#04d3b4",
+      hover_color: "#c2acd4"
+    },
+    PL: {
+      name: "Poland",
+      color: "#04d3b4",
+      hover_color: "#c2acd4"
+    },
+    PT: {
+      name: "Portugal",
+      color: "#04d3b4",
+      hover_color: "#c2acd4"
+    },
+    RO: {
+      name: "Romania",
+      color: "#04d3b4",
+      hover_color: "#c2acd4"
+    },
+    RS: {
+      name: "Serbia",
+      color: "#04d3b4",
+      hover_color: "#c2acd4"
+    },
+    SE: {
+      name: "Sweden",
+      color: "#04d3b4",
+      hover_color: "#c2acd4"
+    },
+    SI: {
+      name: "Slovenia",
+      color: "#04d3b4",
+      hover_color: "#c2acd4"
+    },
+    SK: {
+      name: "Slovakia",
+      color: "#04d3b4",
+      hover_color: "#c2acd4"
+    },
+    TR: {
+      name: "Turkey",
+      color: "#04d3b4",
+      hover_color: "#c2acd4"
+    },
+    UA: {
+      name: "Ukraine",
+      color: "#04d3b4",
+      hover_color: "#c2acd4"
+    }
+  },
+  locations: {
+    "0": {
+      lat: 48.857,
+      lng: 2.341,
+      name: "Paris"
+    }
+  },
+  regions: {},
+  labels: {},
+  legend: {
+    entries: []
+  }
+};
